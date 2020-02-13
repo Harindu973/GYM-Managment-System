11e1a35405217499f8eb366631e61782f72dee7b
Harindu973 <harindulakshankob@gmail.com>
2020-02-14 01:53:46 +0530

    Login Page Backend Done

--- Home/Login.cs
+++ Home/Login.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Employee_Management_System;
 
 namespace Home
 {
     public partial class Login : Form
     {
+
+        SqlConnection constring = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\Asus\OneDrive\Forcus\FO-GYM-Managment-System\Database\Gym.mdf;Integrated Security=True;Connect Timeout=30");
+        private Encap Encap;
+
         public Login()
         {
             InitializeComponent();
@@ -22,5 +28,52 @@ namespace Home
 
         }
 
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            string uname = txtUname.Text;
+            string pw = txtPw.Text;
+            bool Match;
+
+            if (uname == string.Empty || pw == string.Empty)
+            {
+                MessageBox.Show("Please check...  Something's Missing...!!!");
+                //txtUname.Text = "";
+                //txtPw.Text = "";
+            }
+            else
+            {
+                
+                Encap en = new Encap();
+              
+                en.setValues(uname, pw);
+                Match = en.getValues();
+
+                if (Match == false)
+                {
+                    MessageBox.Show("Wrong Username Or Password...!!!");
+                    txtUname.Text = "";
+                    txtPw.Text = "";
+                }
+                else
+                {
+                    Homepage em = new Homepage();
+                    em.Show();
+                    this.Hide();
+
+                }
+            }
+
+
+
+            
+
+
+
+
+
+
+        }
     }
 }
